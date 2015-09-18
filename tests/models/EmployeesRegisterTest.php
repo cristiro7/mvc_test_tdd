ee2b01490e92a0b3aecb906562f4b69009916b59
Thuan Nguyen <thuannguyen@tctav.com>
2015-09-18 10:53:50 +0700

    #1: Edit file test case register user.

--- tests/models/EmployeesRegisterTest.php
+++ tests/models/EmployeesRegisterTest.php
@@ -7,7 +7,7 @@ require_once($HOME . "/library/Model.php");
 require_once($HOME . "/models/EmployeesModel.php");
 
 /**
- * EmployeesLoginTest extends PHPUnit_Framework_TestCase
+ * EmployeesRegisterTest extends PHPUnit_Framework_TestCase
  *
  * This class extends phpUnit.
  * @author ThuanNguyen.
@@ -25,7 +25,7 @@ require_once($HOME . "/models/EmployeesModel.php");
  *   +
  */
 
-class EmployeesLoginTest extends PHPUnit_Framework_TestCase
+class EmployeesRegisterTest extends PHPUnit_Framework_TestCase
 {
     /**
      * Set up data for testcase. in this case it inital EmployeesModel
@@ -44,7 +44,7 @@ class EmployeesLoginTest extends PHPUnit_Framework_TestCase
     /**
      * Test User login by password is not encrypted
      */
-    public function testsfEmployeesLogin_PasswordIsNotEncrypted()
+    public function testsfEmployeesRegister_PasswordIsNotEncrypted()
     {
         // Give the context
         $this->expected = false;
@@ -52,129 +52,6 @@ class EmployeesLoginTest extends PHPUnit_Framework_TestCase
         // When the cause (trigger)
         $username = 'hieu';
         $password = 'hieu';
-        $this->actual = $this->employee->login($username, $password);
-    
-        // Then the effect (assertion)
-        $this->assertEquals($this->expected, $this->actual);
-    }
-    
-    /**
-     * Test User login by password is encrypted MD5
-     */
-    public function testsfEmployeesLogin_PasswordIsEncryptedMD5()
-    {
-        // Give the context
-        $this->expected = false;
-    
-        // When the cause (trigger)
-        $username = 'hieu';
-        $password = md5('hieu');
-        $this->actual = $this->employee->login($username, $password);
-    
-        // Then the effect (assertion)
-        $this->assertEquals($this->expected, $this->actual);
-    }
-    
-    /**
-     * Test User login by password is encrypted SHA-2
-     */
-    public function testsfEmployeesLogin_PasswordIsNotEncryptedSHA2()
-    {
-        // Give the context
-        $this->expected = array(
-            'id'                   => 1,
-            'employeetype_id'      => 1,
-            'username'             => 'hieu',
-            'password'             => 'afc8e16842061ea3dbb023bf5f08d1bc3a728429313fab0cba30f60954ff9064',
-            'lastname'             => 'Hieu',
-            'firstname'            => 'Nguyen',
-            'isaccountant'         => 1
-        );
-    
-        // When the cause (trigger)
-        $username = 'hieu';
-        // Password encrypt by SHA-2
-        $password = hash("sha256", "hieu");
-        $this->actual = $this->employee->login($username, $password);
-    
-        // Then the effect (assertion)
-        $this->assertEquals($this->expected, $this->actual);
-    }
-    
-    /**
-     * Test User login by username and password is match
-     */
-    public function testsfEmployeesLogin_ByUserNameAndPasswordIsMatch()
-    {
-        // Give the context
-        $this->expected = array(
-            'id'                   => 1,
-            'employeetype_id'      => 1,
-            'username'             => 'hieu',
-            'password'             => 'afc8e16842061ea3dbb023bf5f08d1bc3a728429313fab0cba30f60954ff9064',
-            'lastname'             => 'Hieu',
-            'firstname'            => 'Nguyen',
-            'isaccountant'         => 1
-        );
-        
-        // When the cause (trigger)
-        // Password encrypt by SHA-2 
-        $username = 'hieu';
-        $password = hash("sha256", "hieu");
-        $this->actual = $this->employee->login($username, $password);
-        
-        // Then the effect (assertion)
-        $this->assertEquals($this->expected, $this->actual);
-    }
-    
-    /**
-     * Test User login by username is not match
-     */
-    public function testsfEmployeesLogin_ByUserNameIsNotMatch()
-    {
-        // Give the context
-        $this->expected = false;
-    
-        // When the cause (trigger)
-        // Password encrypt by SHA-2
-        $username = 'hieu1';
-        $password = hash("sha256", "hieu");
-        $this->actual = $this->employee->login($username, $password);
-    
-        // Then the effect (assertion)
-        $this->assertEquals($this->expected, $this->actual);
-    }
-    
-    /**
-     * Test User login by password is not match
-     */
-    public function testsfEmployeesLogin_ByPasswordIsNotMatch()
-    {
-        // Give the context
-        $this->expected = false;
-    
-        // When the cause (trigger)
-        // Password encrypt by SHA-2
-        $username = 'hieu';
-        $password = hash("sha256", "hieu1");
-        $this->actual = $this->employee->login($username, $password);
-    
-        // Then the effect (assertion)
-        $this->assertEquals($this->expected, $this->actual);
-    }
-    
-    /**
-     * Test User login by username and password is not match
-     */
-    public function testsfEmployeesLogin_ByUserNameAndPasswordIsNotMatch()
-    {
-        // Give the context
-        $this->expected = false;
-    
-        // When the cause (trigger)
-        // Password encrypt by SHA-2
-        $username = 'hieu1';
-        $password = hash("sha256", "hieu1");
         $this->actual = $this->employee->login($username, $password);
     
         // Then the effect (assertion)
